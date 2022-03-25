222946a66ef05820bc75440f42d3a382da92fbf2
olv-leo <1996olv.leo@gmail.com>
2022-03-25 08:06:43 -0300

    Criando a pasta home direto (sem criar um app para ela)

--- djangotemplatehtmlglobal/djangotemplatehtmlglobal/urls.py
+++ djangotemplatehtmlglobal/djangotemplatehtmlglobal/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('sobre/', include('sobre.urls')),
 ]
+
